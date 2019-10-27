a865059255f5ee3eb6986af1c32319fd50ef35fa
madfol97 <55818559+madfol97@users.noreply.github.com>
2019-10-27 14:18:57 -0400

    page accueil scroll et debut sections

--- front-end/src/accueil.js
+++ front-end/src/accueil.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import styles from './App.module.css';
-
+import cliniquephoto from "./assets/doctors-glass-hallway-127873.jpg";
 
 export const accueil = () => (
   <div className = {styles['Main-page']}>
-      <h2> MHICC - Recrutement pour études cliniques</h2>
-      <p>Le MHICC s'efforce de mener des essais cliniques de façon efficiente et efficace grâce à l'innovation afin de répondre aux besoins de sa clientèle </p>
+    <section id = "accueil">
+      <h1>Welcome</h1>
+      <p>The MHICC strives to conduct clinical trials efficiently and effectively through innocation in order to fulfill our customer needs</p>
+    </section>
+    <section id = "accueilabout">
+      <h1>About the MHICC</h1>
+      <p>he MHICC strives to conduct clinical trials efficiently and effectively through innocation in order to fulfill our customer needshe MHICC strives to conduct clinical trials efficiently and effectively through innocation in order to fulfill our customer needshe MHICC strives to conduct clinical trials efficiently and effectively through innocation in order to fulfill our customer needshe MHICC strives to conduct clinical trials efficiently and effectively through innocation in order to fulfill our customer needshe MHICC strives to conduct clinical trials efficiently and effectively through innocation in order to fulfill our customer needs</p>
+    </section>
+    <section id = "accueilhowitworks">
+      <h1>How it works</h1>
+      <h2>1- Find a study</h2>
+      <p>he MHICC strives to conduct clinical trials efficiently and effectively through innocation in order to fulfill our customer needshe MHICC strives to conduct</p>
+      <h2>2- Fill out an application form</h2>
+      <p>he MHICC strives to conduct clinical trials efficiently and effectively through innocation in order to fulfill our customer needshe MHICC strives to conduct</p>
+      <h2>3- Communicate with a health professionnal</h2>
+      <p>he MHICC strives to conduct clinical trials efficiently and effectively through innocation in order to fulfill our customer needshe MHICC strives to conduct</p>
+    </section>
+    <section id = "accueilclinicalstudies">
+      <h1>Clinical Studies</h1>
+    </section>
   </div>
-)
+  )
 
-
-
+/*  <script src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.js"></script>
+  <script src = "App.js"></script> */
 
 //class accueil extends Component {
   //render() {
@@ -21,5 +39,9 @@ export const accueil = () => (
     //)
   //}
 //}
-
+/*<BackgroundImage source={cliniquephoto} style={{width: '100%', height: '100%'}}>
+<View style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'}}>
+  <Text>Centered text</Text>
+</View>
+</BackgroundImage>*/
 //export default accueil;
