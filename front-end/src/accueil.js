abe41214bdfb8e20b27e5cd60d46f66fd2aa6266
madfol97 <55818559+madfol97@users.noreply.github.com>
2019-10-26 20:11:36 -0400

    main page

--- front-end/src/accueil.js
+++ front-end/src/accueil.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import styles from './App.module.css';
+
+
 export const accueil = () => (
-  <div>
+  <div className = {styles['Main-page']}>
       <h2> MHICC - Recrutement pour études cliniques</h2>
       <p>Le MHICC s'efforce de mener des essais cliniques de façon efficiente et efficace grâce à l'innovation afin de répondre aux besoins de sa clientèle </p>
   </div>
 )
+
+
+
+
 //class accueil extends Component {
   //render() {
     //return (
