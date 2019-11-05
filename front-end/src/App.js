6e8e0faf840c8be6a7082992cd6415d121d814d7
madfol97 <55818559+madfol97@users.noreply.github.com>
2019-11-05 14:48:56 -0500

    Revert "restructure des fichiers et fichier css"
    This reverts commit 20c514a9ad1e4fbb369cee52b116a1e2dfb6efba.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { accueil } from "./accueil";
+import { fonctionnement } from "./fonctionnement";
+import { etudescliniques } from "./etudescliniques";
+import { apropos } from "./apropos";
+import { questionnairepreliminaire } from "./questionnairepreliminaire";
+import { NoMatch } from "./NoMatch";
+import { Layout } from "./Components/Layout";
+import { NavigationBar } from "./Components/NavigationBar";
+import { faq } from "./faq";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavigationBar />
+        <Layout>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={accueil} />
+              <Route path="/etudescliniques" component={etudescliniques} />
+              <Route
+                path="/questionnairepreliminaire"
+                component={questionnairepreliminaire}
+              />
+              <Route path="/apropos" component={apropos} />
+              <Route path="/fonctionnement" component={fonctionnement} />
+              <Route path="/faq" component={faq} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Router>
+        </Layout>
+      </React.Fragment>
+    );
+  }
 }
-
 export default App;
