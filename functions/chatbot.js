2425b3525793dec820c7522617eb2536372f129a
Unknown <zacharie@hudsonlegault.net>
2019-11-05 22:41:00 -0500

    Webhook to retrieve study names from Firebase
    WIP. Gives a request timeout

---
+++ functions/chatbot.js
@@ -0,0 +1,46 @@
+// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
+// for Dialogflow fulfillment library docs, samples, and to report issues
+'use strict';
+ 
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+let db = admin.firestore();
+
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+ 
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+ 
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+ 
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+  
+  function getCurrentTrials(agent) {
+    agent.add(`Let me check...`);
+    let clinicalStudyIDs = db.collection("clinicalStudies");
+    const studiesPromise = clinicalStudyIDs.get();
+    studiesPromise.forEach( doc => {
+        if (doc.data().name){
+            agent.add(doc.data().name);
+        }
+    });
+  }
+
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  intentMap.set('currentTrials', getCurrentTrials);
+  agent.handleRequest(intentMap);
+});
