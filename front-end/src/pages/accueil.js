a7b1607565c00149c0d07657232995deebb8230b
madfol97 <55818559+madfol97@users.noreply.github.com>
2019-11-06 18:27:23 -0500

    page d'accueil redesign pour pouvoir fitter avec le css

--- front-end/src/pages/accueil.js
+++ front-end/src/pages/accueil.js
@@ -32,82 +32,53 @@ export const accueil = () => (
             needshe MHICC strives to conduct clinical trials efficiently and
             effectively through innocation in order to fulfill our customer
             needs</p>
-        href="/apropos">Venez lire sur notre mission
+      <Button variant = "outline-light" href = "/apropos"> Venez lire sur notre mission</Button>
     </section>
     <br />
     <section id="accueilhowitworks">
-      <Card className="text-center" bg="info" text="white">
-        <Card.Body>
-          <Card.Title>
-            How it works/Le fonctionnement des études cliniques
-          </Card.Title>
-          <Card.Text>
-            Les études cliniques sont une part essentielle du développement de
+      <h1>How it works/Le fonctionnement des études cliniques</h1>
+        <p>Les études cliniques sont une part essentielle du développement de
             nouveaux traitements. Malheureusement, recruter des patients
-            représente un défi.
-            <h2>1- Find a study</h2>
-            <p>
-              he MHICC strives to conduct clinical trials efficiently and
+            représente un défi.</p>
+      <h2>1- Find a study</h2>
+        <p>The MHICC strives to conduct clinical trials efficiently and
+            effectively through innocation in order to fulfill our customer
+            needshe MHICC strives to conduct</p>
+      <h2>2- Fill out an application form</h2>
+        <p>The MHICC strives to conduct clinical trials efficiently and
+              effectively through innocation in order to fulfill our customer
+              needshe MHICC strives to conduct</p>
+      <h2>3- Communicate with a health professionnal</h2>
+        <p>The MHICC strives to conduct clinical trials efficiently and
               effectively through innocation in order to fulfill our customer
               needshe MHICC strives to conduct
-            </p>
-            <h2>2- Fill out an application form</h2>
-            <p>
-              he MHICC strives to conduct clinical trials efficiently and
-              effectively through innocation in order to fulfill our customer
-              needshe MHICC strives to conduct
-            </p>
-            <h2>3- Communicate with a health professionnal</h2>
-            <p>
-              he MHICC strives to conduct clinical trials efficiently and
-              effectively through innocation in order to fulfill our customer
-              needshe MHICC strives to conduct
-            </p>
-          </Card.Text>
-          <Card.Link href="/fonctionnement">
+            </p> href="/fonctionnement">
             En apprendre plus sur le cycle de vie des études cliniques
-          </Card.Link>
-        </Card.Body>
-      </Card>
       <br />
     </section>
 
     <section id="accueilclinicalstudies">
-      <Card className="text-center" bg="secondary" text="white">
-        <Card.Body>
-          <Card.Title>Nos études cliniques</Card.Title>
-          <Card.Text>
+      <h1>Nos études cliniques</h1>
+          <p>
             Plusieurs études sont actuellement en cours de recrutement. Vous
             êtes intéressés? Vous pouvez accéder à notre page dédiée aux études
             cliniques ou faire le quiz interactif pour découvrir si vous êtes
             admissible!
-          </Card.Text>
-          <Card.Link
-            href="/etudescliniques"
-            text="white"
-            id="accueilclinicalstudies"
-          >
-            Voir les études cliniques
-          </Card.Link>{" "}
-          <Button href="/questionnairepreliminaire" variant="primary">
-            Faire le quiz
-          </Button>
-        </Card.Body>
-      </Card>
+          </p>
+          <Button href="/etudescliniques" variant = "outline-dark">Voir les études cliniques</Button>
+          <Button href="/questionnairepreliminaire" variant="primary" size = "lg">Faire le quiz</Button>
       <br />
-
-      <Card className="text-center" bg="danger" text="white">
-        <Card.Body>
-          <Card.Title>FAQ</Card.Title>
-          <Card.Text>
-            Nous sommes là pour vous aider dans votre de choix de participer ou
+    </section>
+    <section>
+      <h1>Foire aux questions</h1>
+          <p>
+          Nous sommes là pour vous aider dans votre de choix de participer ou
             non aux études cliniques. Vous avez probablement 1001 questions pour
             nous. Voilà pourquoi une section de notre site est réservé aux
             questions les plus fréquemment posées.
-          </Card.Text>
-          <Card.Link href="/apropos">Voir la foire aux questions</Card.Link>
-        </Card.Body>
-      </Card>
+          </p>
+          <Button href="/faq" variant = "outline-dark">Voir la foire aux questions</Button>
+      <br />
     </section>
     <br />
   </div>
